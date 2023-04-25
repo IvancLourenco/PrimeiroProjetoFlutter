81d95c59283184a221631480d2644ec0c0d7f1f2
Ivan Lourenco <ivancampos039@gmail.com>
2023-04-25 20:08:28 -0300

    novas atualizacoes

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,19 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
     title: "Projeto Container",
-    home:Container(
-      color: Colors.white,
-      child:const Column(
-        children: [
-          Text("Text 01"),
-          Text("Text 02"),
-          Text("Text 03")
-        ],
-      )
-
-    )
+    home:Scaffold(
+      appBar: AppBar(
+        title:Text("Exemplo Scaffold"),
+        backgroundColor: Colors.black, 
+        ),
+        body: Container(
+          child: const Column(
+            children: [
+              Text("Teste de scaffold")
+            ],
+          ),
+        ),
+        )
 
   ));
 }
